0d45c3c5a7d88aadd3afc467fd1da2eee2428364
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-04-25 15:30:56 -0300

    Se realizo una limpieza de archivos que ya no eran necesario y se afrego el formulario  mas una hoja html de Validacion de Datos

--- js/index.js
+++ js/index.js
@@ -19,12 +19,6 @@
 
 /* Section de la Pagina  */
 
-fetch("/body/section.html")
-  .then(response => response.text())
-  .then(data => {
-    const sectionContainer = document.getElementById('section-container');
-    sectionContainer.innerHTML = data;
-  });
 
 /* Aside de la Pagina  */
 
@@ -44,44 +38,3 @@ fetch("/body/section.html")
     footerContainer.innerHTML = data;
   });
 
-
-/*
-
-<button onclick="mostrarContenedor('contenedor1')">Mostrar contenedor 1</button>
-<button onclick="mostrarContenedor('contenedor2')">Mostrar contenedor 2</button>
-<button onclick="mostrarContenedor('contenedor3')">Mostrar contenedor 3</button>
-<div class="contenedor-padre">
-<div class="contenedor" id="contenedor1">
-<p>Contenido del contenedor 1</p>
-</div>
-<div class="contenedor" id="contenedor2">
-<p>Contenido del contenedor 2</p>
-</div>
-<div class="contenedor" id="contenedor3">
-<p>Contenido del contenedor 3</p>
-</div>
-</div>
-
-
-  function mostrarContenedor(id) {
-  // Oculta todos los contenedores
-  var contenedores = document.querySelectorAll('.contenedor');
-  contenedores.forEach(function(contenedor) {
-    contenedor.classList.remove('activo');
-  });
-
-  // Muestra el contenedor seleccionado
-  var contenedorActivo = document.getElementById(id);
-  contenedorActivo.classList.add('activo');
-};
-
-.contenedor {
-  display: none;
-}
-
-.contenedor.activo {
-  display: block;
-}
-
-
-*/
